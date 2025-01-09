c9a29722c22a22cbacb7b40fc2029762fcc38501
Lorenz Boss <lorenz.boss@bbzbl-it.ch>
2025-01-09 21:54:56 +0100

    Format tailwindcss code with prettier

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,16 +38,16 @@ export default function RootLayout({
       <body
         className={clsx(
           "min-h-screen bg-background font-sans antialiased",
-          fontSans.variable
+          fontSans.variable,
         )}
       >
         <Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
-          <div className="relative flex flex-col h-screen">
+          <div className="relative flex h-screen flex-col">
             <Navbar />
-            <main className="w-full mx-auto max-w-7xl pt-16 px-12 flex-grow">
+            <main className="mx-auto w-full max-w-7xl flex-grow px-12 pt-16">
               {children}
             </main>
-            <footer className="w-full flex items-center justify-center py-3">
+            <footer className="flex w-full items-center justify-center py-3">
               <Link
                 isExternal
                 className="flex items-center gap-1 text-current"
--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,8 @@ import { subtitle, title } from "@/components/primitives";
 
 export default function Home() {
   return (
-    <div className="items-center flex flex-col">
-      <div className="inline-block max-w-xl text-center justify-center">
+    <div className="flex flex-col items-center">
+      <div className="inline-block max-w-xl justify-center text-center">
         <span className={title()}>Make&nbsp;</span>
         <span className={title({ color: "violet" })}>beautiful&nbsp;</span>
         <br />
@@ -18,7 +18,7 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="mt-8 ">
+      <div className="mt-8">
         <Snippet hideSymbol variant="bordered">
           <span>
             Get started by editing <Code color="primary">app/page.tsx</Code>
