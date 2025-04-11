5fef1f8d7cfb2b927c4a543a0db9607c2f8cccd4
Lorenz Boss <lorenz.boss@bbzbl-it.ch>
2025-04-11 12:24:54 +0200

    Add icon components and utility functions

--- lib/definitions.ts
+++ lib/definitions.ts
@@ -1,5 +1,11 @@
 import { SVGProps } from "react";
 
+export type Artist = {
+  id: string;
+  name: string;
+  images: { url: string }[];
+};
+
 export type IconSvgProps = SVGProps<SVGSVGElement> & {
   size?: number;
 };
