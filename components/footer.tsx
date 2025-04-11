131031d6ee1d6ae1ff0310eb135cd2c70fb13f99
Lorenz Boss <lorenz.boss@bbzbl-it.ch>
2025-04-11 12:25:24 +0200

    Add Footer component

---
+++ components/footer.tsx
@@ -0,0 +1,25 @@
+import { Link } from "@heroui/link";
+import { HeartFilledIcon } from "./icons";
+
+export default function Footer() {
+  return (
+    <footer className="flex w-full flex-col items-center space-y-2 py-1">
+      <p>
+        View and edit this code on{" "}
+        <Link isExternal href="https://github.com/lorenzboss/portfolio">
+          GitHub
+        </Link>
+        .
+      </p>
+
+      <p>
+        Made with <HeartFilledIcon className="inline text-red-500 opacity-80" />{" "}
+        and{" "}
+        <Link isExternal href="https://www.heroui.com/">
+          HeroUI
+        </Link>{" "}
+        | {new Date().getFullYear()} Lorenz Boss
+      </p>
+    </footer>
+  );
+}
