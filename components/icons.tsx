2ae920a8f8e059da2c473c21a3e4deb1c0148320
Lorenz Boss <lorenz.boss@bbzbl-it.ch>
2025-07-02 21:22:43 +0200

    Add RecordIcon

--- components/icons.tsx
+++ components/icons.tsx
@@ -144,12 +144,23 @@ export const MusicIcon = ({ size = 24, ...props }: IconSvgProps) => (
     aria-hidden="true"
     fill="#A1A1AA"
     focusable="false"
-    height={size}
     role="presentation"
     viewBox="0 0 24 24"
+    height={size}
     width={size}
     {...props}
   >
     <path d="M12 3v10.55c-.59-.34-1.27-.55-2-.55-2.21 0-4 1.79-4 4s1.79 4 4 4 4-1.79 4-4V7h4V3h-6z" />
+  </svg>
+);
+
+export const RecordIcon = ({ ...props }: IconSvgProps) => (
+  <svg
+    className="h-16 w-16 text-default-400"
+    fill="currentColor"
+    viewBox="0 0 24 24"
+    {...props}
+  >
+    <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 14.5c-2.49 0-4.5-2.01-4.5-4.5S9.51 7.5 12 7.5s4.5 2.01 4.5 4.5-2.01 4.5-4.5 4.5zm0-5.5c-.55 0-1 .45-1 1s.45 1 1 1 1-.45 1-1-.45-1-1-1z" />
   </svg>
 );
